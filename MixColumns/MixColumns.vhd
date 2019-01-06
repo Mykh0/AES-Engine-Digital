cac685bbcaf7b4455d13f34741789263ef41322b
Unknown <michael_chan@live.nl>
2019-01-06 14:20:35 +0100

    MixColumns VHDL **NOT TESTED**
    TODO: MixColumns Testbench
    .gitignore fix

--- MixColumns/MixColumns.vhd
+++ MixColumns/MixColumns.vhd
@@ -49,7 +49,11 @@ end MULTIPLY_ONE;
 
 function MULTIPLY_TWO(A : STD_LOGIC_VECTOR(7 downto 0) := x"00") return STD_LOGIC_vECTOR is
 begin
-  return (A(6 downto 0) & '0') xor x"1B";
+  if (A(7) = '1') then
+    return (A(6 downto 0) & '0') xor x"1B";
+  else
+    return (A(6 downto 0) & '0');
+  end if;
 end MULTIPLY_TWO;
   
 function MULTIPLY_THREE(A : STD_LOGIC_VECTOR(7 downto 0) := x"00") return STD_LOGIC_VECTOR is
@@ -57,28 +61,38 @@ begin
   return MULTIPLY_TWO(A) xor A;
 end MULTIPLY_THREE;
 
-function MIX_ONE_COLUMN(A : STD_LOGIC_VECTOR(31 downto 0) := x"00") return STD_LOGIC_VECTOR is
-  variable byteA : STD_LOGIC_VECTOR(7 downto 0) := A(31 downto 24);
-  variable byteB : STD_LOGIC_VECTOR(7 downto 0) := A(23 downto 16);
-  variable byteC : STD_LOGIC_VECTOR(7 downto 0) := A(15 downto 8);
-  variable byteD : STD_LOGIC_VECTOR(7 downto 0) := A(7 downto 0);
+function MIX_ONE_COLUMN(A : STD_LOGIC_VECTOR(31 downto 0) := x"00000000") return STD_LOGIC_VECTOR is
+  variable byteA : STD_LOGIC_VECTOR(7 downto 0) := x"00";
+  variable byteB : STD_LOGIC_VECTOR(7 downto 0) := x"00";
+  variable byteC : STD_LOGIC_VECTOR(7 downto 0) := x"00";
+  variable byteD : STD_LOGIC_VECTOR(7 downto 0) := x"00";
+  variable byteAEcrypted : STD_LOGIC_VECTOR(7 downto 0) := x"00";
+  variable byteBEcrypted : STD_LOGIC_VECTOR(7 downto 0) := x"00";
+  variable byteCEcrypted : STD_LOGIC_VECTOR(7 downto 0) := x"00";
+  variable byteDEcrypted : STD_LOGIC_VECTOR(7 downto 0) := x"00";
 begin
-  byteA := MULTIPLY_TWO(byteA) xor MULTIPLY_THREE(byteA) xor MULTIPLY_ONE(byteA) xor MULTIPLY_ONE(byteA);
-  byteB := MULTIPLY_ONE(byteB) xor MULTIPLY_TWO(byteB) xor MULTIPLY_THREE(byteB) xor MULTIPLY_ONE(byteB);
-  byteC := MULTIPLY_ONE(byteC) xor MULTIPLY_ONE(byteC) xor MULTIPLY_TWO(byteC) xor MULTIPLY_THREE(byteC);
-  byteD := MULTIPLY_THREE(byteD) xor MULTIPLY_ONE(byteD) xor MULTIPLY_ONE(byteD) xor MULTIPLY_TWO(byteD);
-  return byteA xor byteB xor byteC xor byteD;
+  byteA := A(31 downto 24);
+  byteB := A(23 downto 16);
+  byteC := A(15 downto 8);
+  byteD := A(7 downto 0);
+  byteAEcrypted := MULTIPLY_TWO(byteA) xor MULTIPLY_THREE(byteB) xor MULTIPLY_ONE(byteC) xor MULTIPLY_ONE(byteD);
+  byteBEcrypted := MULTIPLY_ONE(byteA) xor MULTIPLY_TWO(byteB) xor MULTIPLY_THREE(byteC) xor MULTIPLY_ONE(byteD);
+  byteCEcrypted := MULTIPLY_ONE(byteA) xor MULTIPLY_ONE(byteB) xor MULTIPLY_TWO(byteC) xor MULTIPLY_THREE(byteD);
+  byteDEcrypted := MULTIPLY_THREE(byteA) xor MULTIPLY_ONE(byteB) xor MULTIPLY_ONE(byteC) xor MULTIPLY_TWO(byteD);
+  return byteAEcrypted & byteBEcrypted & byteCEcrypted & byteDEcrypted;
 end MIX_ONE_COLUMN;
 
 BEGIN
 
 MixDatBish : process(RawData)
 begin
-  Encrypted <= MIX_ONE_COLUMN(RawData(127 downto 120))
-               MIX_ONE_COLUMN(RawData(120 downto 113))
---					& MIX_ONE_COLUMN(RawData(63 downto 32))
---					& MIX_ONE_COLUMN(RawData(31 downto 0));
-               & x"000000000000000000000000";
+--  for I in 16 downto 0 loop
+--    Encrypted(7+I*8 downto I*8) <= MIX_ONE_COLUMN(7+I*8 downto I*8);
+--  end loop;
+  Encrypted <= MIX_ONE_COLUMN(RawData(127 downto 96))
+               & MIX_ONE_COLUMN(RawData(95 downto 64))
+					& MIX_ONE_COLUMN(RawData(63 downto 32))
+					& MIX_ONE_COLUMN(RawData(31 downto 0));
 end process;
 
 END MixColumns_architecture;
